59a7cb801a8ce01173247ac99ea9e7f2d95e9e58
Rolando Morales <rmoralespp@gmail.com>
2018-01-04 17:57:28 +0100

    Se cambio la referencia a la funcion init del Objeto

--- js/main.js
+++ js/main.js
@@ -1,38 +1,30 @@
 
-$(() => init());
+$(() => Objeto.init());
 
 Objeto = {
-    init: function(){return this;}
-}
+   template : null,
+   init : function() {
+    let source = $("#template").html();
+    this.template = Handlebars.compile(source);
+    loadTemplates(this.template);
+    }
+}    
 
-
-let init = function(){
-    let source   = $("#template").html();
-    let template = Handlebars.compile(source);
-    objeto = Objeto.init()
-    objeto.template = template;
-    loadTemplates(template);
-   
-    
-}
-
-
-
-
-let loadPaises = function(idioma = 'en'){
-    template = Objeto.template || null;
+let loadPaises = function(idioma = 'en') {
+    template = Objeto.template;
     idiomas = {
         'en': "https://restcountries.eu/rest/v2/all",
         'es': "https://restcountries.eu/rest/v2/lang/es",
     }
     let api_url = idiomas[idioma];
        
-    let http_api = $.ajax({
-        url: api_url,
-        method: 'GET',
-        dataType: "JSON",
-     });
+    
     if(template){
+        let http_api = $.ajax({
+            url: api_url,
+            method: 'GET',
+            dataType: "JSON",
+         });
         render_http(http_api, template);
     }
    
@@ -40,7 +32,7 @@ let loadPaises = function(idioma = 'en'){
 
 
 
-let render_http = function(http, template, template_parent= $("#contenido")){   
+let render_http = function(http, template, template_parent= $("#contenido")) {   
     http.done(
             (data) => {
                 context = {'paises':data }
@@ -50,7 +42,7 @@ let render_http = function(http, template, template_parent= $("#contenido")){
     }
 
 
-let render = function(template, template_parent= $("#contenido")){
+let render = function(template, template_parent= $("#contenido")) {
     let html = template({'paises':[]});
     template_parent.html(html);
 }
